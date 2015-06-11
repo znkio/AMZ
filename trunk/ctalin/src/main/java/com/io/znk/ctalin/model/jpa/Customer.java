88126abc928a76b86fbf923e2d966926c9dcda17
aadamopoulos <aadamopoulos@6dd4b84f-5050-4c00-bd35-72060c409549>
2015-06-11 12:21:49 +0000

    Customer & ReceiptData POJOs

---
+++ trunk/ctalin/src/main/java/com/io/znk/ctalin/model/jpa/Customer.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.io.znk.ctalin.model.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author adamopoa
+ */
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private Long customerId;
+
+    @NotNull
+    @Column(unique = true)
+    private Long customerAFM;
+
+    @Column
+    private String customerFname;
+
+    @Column
+    private String customerLname;
+
+    
+    public Customer(Long customerId, Long customerAFM, String customerFname, String customerLname) {
+        this.customerId = customerId;
+        this.customerAFM = customerAFM;
+        this.customerFname = customerFname;
+        this.customerLname = customerLname;
+    }
+    
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public Long getCustomerAFM() {
+        return customerAFM;
+    }
+
+    public String getCustomerFname() {
+        return customerFname;
+    }
+
+    public String getCustomerLname() {
+        return customerLname;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setCustomerAFM(Long customerAFM) {
+        this.customerAFM = customerAFM;
+    }
+
+    public void setCustomerFname(String customerFname) {
+        this.customerFname = customerFname;
+    }
+
+    public void setCustomerLname(String customerLname) {
+        this.customerLname = customerLname;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Customer{" + "customerId=" + customerId + ", customerAFM=" + customerAFM + ", customerFname=" + customerFname + ", customerLname=" + customerLname + '}';
+    }
+        
+        
+        
+}
