334fa3b832ecc54c78f2d9857ddb64b89c94fbe2
gchourdakis <gchourdakis@6dd4b84f-5050-4c00-bd35-72060c409549>
2015-06-13 16:32:57 +0000

    added qr generator and a controller

--- src/main/java/com/io/znk/ctalin/service/AchievementcustomerService.java
+++ src/main/java/com/io/znk/ctalin/service/AchievementcustomerService.java
@@ -1,16 +1,18 @@
 package com.io.znk.ctalin.service;
 
 import com.io.znk.ctalin.model.jpa.Achievementcustomer;
+import com.io.znk.ctalin.model.jpa.Customer;
 import java.util.List;
-
 
 public interface AchievementcustomerService {
 
-	List<Achievementcustomer> findAll();
-        
-        boolean deleteAchievementcustomer(Achievementcustomer ach);
-        
-        Achievementcustomer updateAchievementcustomer(Achievementcustomer ach);
-        
-        Achievementcustomer findAchievementcustomer(Achievementcustomer ach);
+    List<Achievementcustomer> findAll();
+
+    boolean deleteAchievementcustomer(Achievementcustomer ach);
+
+    Achievementcustomer updateAchievementcustomer(Achievementcustomer ach);
+
+    Achievementcustomer findAchievementcustomer(Achievementcustomer ach);
+
+    List<Achievementcustomer> findByCustomerID(Customer customerID);
 }
--- src/main/java/com/io/znk/ctalin/web/api/AchievementController.java
+++ src/main/java/com/io/znk/ctalin/web/api/AchievementController.java
@@ -1,7 +1,10 @@
 package com.io.znk.ctalin.web.api;
 
 import com.io.znk.ctalin.model.jpa.Achievement;
+import com.io.znk.ctalin.model.jpa.Achievementcustomer;
+import com.io.znk.ctalin.model.jpa.Customer;
 import com.io.znk.ctalin.service.AchievementService;
+import com.io.znk.ctalin.service.AchievementcustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,8 @@ public class AchievementController {
 
     @Autowired
     AchievementService as;
+    @Autowired
+    AchievementcustomerService acs;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ResponseEntity<List<Achievement>> getachs() {
@@ -35,13 +40,40 @@ public class AchievementController {
         return new ResponseEntity(this.as.updateAchievement(ach), HttpStatus.OK);
     }
 
-    @RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity delach(@PathVariable String id) {
-        if(id==null||id.equals("")){
-            return new ResponseEntity("Cannot delete with empty id.",HttpStatus.BAD_REQUEST);
+        if (id == null || id.equals("")) {
+            return new ResponseEntity("Cannot delete with empty id.", HttpStatus.BAD_REQUEST);
         }
-        Achievement ach=new Achievement(id);
+        Achievement ach = new Achievement(id);
         return new ResponseEntity(this.as.deleteAchievement(ach), HttpStatus.OK);
     }
-    
+
+    @RequestMapping(value = "/forcustomer/{customerId}", method = RequestMethod.GET)
+    public ResponseEntity<List<Achievementcustomer>> getachcs(@PathVariable String customerId) {
+        if (customerId == null || customerId.equals("")) {
+            return new ResponseEntity("Cannot find staff for customer with empty id.", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity(this.acs.findByCustomerID(new Customer(customerId)), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/forcustomer/{customerId}", method = RequestMethod.PUT)
+    public ResponseEntity<Achievementcustomer> putachcs(@PathVariable String customerId, @RequestBody Achievementcustomer achievementcustomer) {
+        if (achievementcustomer == null) {
+            return new ResponseEntity("Cannot find staff for customer with empty id.", HttpStatus.BAD_REQUEST);
+        }
+        Customer cust = new Customer(customerId);
+        achievementcustomer.setCustomerID(cust);
+        return new ResponseEntity(this.acs.updateAchievementcustomer(achievementcustomer), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/forcustomer/{customerId}", method = RequestMethod.POST)
+    public ResponseEntity<Achievementcustomer> postachcs(@PathVariable String customerId, @RequestBody Achievementcustomer achievementcustomer) {
+        if (achievementcustomer == null) {
+            return new ResponseEntity("Cannot find staff for customer with empty id.", HttpStatus.BAD_REQUEST);
+        }
+        Customer cust = new Customer(customerId);
+        achievementcustomer.setCustomerID(cust);
+        return new ResponseEntity(this.acs.updateAchievementcustomer(achievementcustomer), HttpStatus.OK);
+    }
 }
