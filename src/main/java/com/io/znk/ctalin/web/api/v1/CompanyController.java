f52a9f44288046c946e9715e3fbbb05f2459e437
alexius.diakogiannis <alexius.diakogiannis@6dd4b84f-5050-4c00-bd35-72060c409549>
2015-06-13 09:23:49 +0000

    Deployment on all servers.

--- src/main/java/com/io/znk/ctalin/web/api/v1/CompanyController.java
+++ src/main/java/com/io/znk/ctalin/web/api/v1/CompanyController.java
@@ -5,10 +5,29 @@
  */
 package com.io.znk.ctalin.web.api.v1;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  *
  * @author adamopoa
  */
+
+@Controller
+@RequestMapping(value="/demo")       
 public class CompanyController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CompanyController.class);
+
+    
     
 }
