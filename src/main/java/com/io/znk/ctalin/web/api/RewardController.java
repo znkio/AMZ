c4684c79f0e5aa21e83ae76843fbc5660f9425f5
aadamopoulos <aadamopoulos@6dd4b84f-5050-4c00-bd35-72060c409549>
2015-06-13 15:52:46 +0000

    RewardController

---
+++ src/main/java/com/io/znk/ctalin/web/api/RewardController.java
@@ -0,0 +1,47 @@
+package com.io.znk.ctalin.web.api;
+
+import com.io.znk.ctalin.model.jpa.Rewards;
+import com.io.znk.ctalin.service.RewardsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+@RequestMapping(value = "/mobile/api/rewards")
+public class RewardController {
+
+    @Autowired
+    RewardsService rs;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<List<Rewards>> getRewards() {
+
+        return new ResponseEntity(this.rs.findAll(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public ResponseEntity<Rewards> updateach(@RequestBody Rewards rwd) {
+        return new ResponseEntity(this.rs.updateRewards(rwd), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Rewards> createach(@RequestBody Rewards rwd) {
+        return new ResponseEntity(this.rs.updateRewards(rwd), HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+    public ResponseEntity delach(@PathVariable String id) {
+        if(id==null||id.equals("")){
+            return new ResponseEntity("Cannot delete with empty id.",HttpStatus.BAD_REQUEST);
+        }
+        Rewards rwd = new Rewards(id);
+        return new ResponseEntity(this.rs.deleteRewards(rwd), HttpStatus.OK);
+    }
+    
+}
